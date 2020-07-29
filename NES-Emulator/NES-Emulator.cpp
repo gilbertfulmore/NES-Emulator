579b1c93847530dcd3436e4c2ad92db6c2765172
Gilbert Fulmore <gilbertfulmore@gmail.com>
2020-07-28 22:02:18 -0700

    Initialize Project in Visual Studio
    Initializing project using Visual Studio 2019

---
+++ NES-Emulator/NES-Emulator.cpp
@@ -0,0 +1,16 @@
+/**
+ *
+ * A Nintendo Entertainment System Emulator for Windows
+ *
+ * @author  Gilbert Fulmore
+ * @date    2020-07-28
+ * @version 0.001
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
